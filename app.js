43e55fd859fb5d44cc58fe5e4a816b024de2010f
Exemption6877 <106035951+Exemption6877@users.noreply.github.com>
2025-05-22 22:14:01 +0300

    Add messages entries

--- app.js
+++ app.js
@@ -3,6 +3,34 @@ const app = express();
 
 const PORT = 3000;
 
+const messages = [
+  {
+    text: "Good morning, everyone!",
+    user: "Dana",
+    added: new Date(),
+  },
+  {
+    text: "How's it going?",
+    user: "Eli",
+    added: new Date(),
+  },
+  {
+    text: "Just joined the chat!",
+    user: "Fiona",
+    added: new Date(),
+  },
+  {
+    text: "Excited to be here.",
+    user: "George",
+    added: new Date(),
+  },
+  {
+    text: "What's the topic today?",
+    user: "Harper",
+    added: new Date(),
+  },
+];
+
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
