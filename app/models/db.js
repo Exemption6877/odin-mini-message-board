5d11edb1b20469d0daa79063567d8a9142315620
Exemption6877 <106035951+Exemption6877@users.noreply.github.com>
2025-05-23 07:34:26 +0300

    Randomize dates

--- app/models/db.js
+++ app/models/db.js
@@ -1,28 +1,28 @@
 const messages = [
   {
+    text: "Just joined the chat!",
+    user: "Fiona",
+    added: new Date(2024, 1, 21, 19, 5),
+  },
+  {
     text: "Good morning, everyone!",
     user: "Dana",
-    added: new Date(),
+    added: new Date(2023, 4, 12, 8, 15),
   },
   {
     text: "How's it going?",
     user: "Eli",
-    added: new Date(),
-  },
-  {
-    text: "Just joined the chat!",
-    user: "Fiona",
-    added: new Date(),
+    added: new Date(2022, 10, 3, 14, 45),
   },
   {
     text: "Excited to be here.",
     user: "George",
-    added: new Date(),
+    added: new Date(2021, 6, 9, 10, 30),
   },
   {
     text: "What's the topic today?",
     user: "Harper",
-    added: new Date(),
+    added: new Date(2020, 0, 27, 16, 0),
   },
 ];
 
