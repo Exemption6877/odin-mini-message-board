04b944a75678a17d71f6ed8bbd91ad16a3fca1bb
Exemption6877 <106035951+Exemption6877@users.noreply.github.com>
2025-05-23 20:08:52 +0300

    Fix path to views/

--- app/app.js
+++ app/app.js
@@ -2,9 +2,13 @@ const express = require("express");
 const app = express();
 
 const messageRouter = require("./routes/messageRouter");
+
 const PORT = 3000;
 
+const path = require("path");
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/", messageRouter);
